c7303a058ab849833845680496a855c383385095
Shuvo <suvo10.info@gmail.com>
2025-08-18 01:35:06 +0600

    bitwise

---
+++ bitwise.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+img1=cv2.imread('1bit1.png')
+img2=cv2.imread('2bit2.png')
+dest_and = cv2.bitwise_and(img2,img1,mask=None)
+cv2.imshow('Bitwise And',dest_and)
+if cv2.waitKey(0) & 0xff==27:
+    cv2.destroyAllWindows()
