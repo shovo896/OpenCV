90d7a2fd973c30b0a8800b1fb3bcd22101a462ee
Shuvo <suvo10.info@gmail.com>
2025-08-19 22:40:54 +0600

    color

---
+++ thresholding.py
@@ -0,0 +1,10 @@
+import cv2 
+import numpy as np
+image1=cv2.imread('1bit1.png')
+img=cv2.cvtColor(image1,cv2.COLOR_BGR2GRAY)
+ret,thresh1=cv2.threshold(img,120,255,cv2.THRESH_BINARY)
+ret,thresh2=cv2.threshold(img,120,255,cv2.THRESH_BINARY_INV)
+cv2.imshow('Binary Threshold',thresh1)
+cv2.imshow('Binary Threshold Inverted',thresh2)
+if cv2.waitKey(0) & 0xff==27:
+    cv2.destroyAllWindows()
