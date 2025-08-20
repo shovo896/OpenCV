b7a70862f53831daab2383a1a30330760deedda5
Shuvo <suvo10.info@gmail.com>
2025-08-20 20:55:24 +0600

    dxiff

--- edgedtection.py
+++ edgedtection.py
@@ -1,7 +1,7 @@
 import cv2
 FILE_NAME='2bit2.png'
 img=cv2.imread(FILE_NAME)
-edges=cv2.canny(img,100,200)
+edges=cv2.Canny(img,100,200)
 cv2.imshow('Edges',edges)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
