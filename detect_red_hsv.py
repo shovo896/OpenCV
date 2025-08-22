8ca58acd2c5126bb18521e3e8bd495e3386470f6
Shuvo <suvo10.info@gmail.com>
2025-08-22 15:38:47 +0600

    diff

--- detect_red_hsv.py
+++ detect_red_hsv.py
@@ -1,12 +1,12 @@
 import cv2 
 import numpy as np
-from google.colab.patches import cv2_imshow
+
 image=cv2.imread('1bit1.png')
 image_hsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
 lower_red=np.array([0,120,70])
 upper_red=np.array([10,255,255])
 mask=cv2.inRange(image_hsv,lower_red,upper_red)
 result=cv2.bitwise_and(image,image,mask=mask)
-cv2_imshow(result)
+cv2.imshow(result)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
