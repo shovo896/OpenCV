fc36dbf0af71ad51d6d30e2494a94e22fc8294f1
Shuvo <suvo10.info@gmail.com>
2025-08-22 17:33:18 +0600

    median

--- blurring.py
+++ blurring.py
@@ -4,9 +4,9 @@ image_path = '1bit1.png'
 image=cv2.imread(image_path)
 resized_image=cv2.resize(image,(1900,800))
 resized_image_rgb=cv2.cvtColor(resized_image,cv2.COLOR_BGR2RGB)
-
-Gaussian=cv2.GaussianBlur(resized_image,(15,15),0)
-Gaussian_rgb=cv2.cvtColor(Gaussian,cv2.COLOR_BGR2RGB)
-plt.imshow(Gaussian_rgb)
-plt.axis('off')
+median=cv2.medianBlur(resized_image,11)
+median_rgb=cv2.cvtColor(median,cv2.COLOR_BGR2RGB)
+plt.imshow(median_rgb)
+plt.axis('Off')
 plt.show()
+
