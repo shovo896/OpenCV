f398a930582aa82e57462d231e7e7071dd1184a9
Shuvo <suvo10.info@gmail.com>
2025-08-22 17:54:57 +0600

    flag=0 diya

--- gray_sacling.py
+++ gray_sacling.py
@@ -1,9 +1,7 @@
 import cv2
-image=cv2.imread('img_5terre.jpg')
+img=cv2.imread('img_5terre.jpg',0)
 cv2.imshow('Orginal',image)
-cv2.waitKey(0)
 
-gray_image=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-cv2.imshow('Grayscale',gray_image)
+cv2.imshow('Grayscale',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
