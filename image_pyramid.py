932795d10b3487c549139875aa6cf5ab8004992a
Shuvo <suvo10.info@gmail.com>
2025-08-25 22:27:31 +0600

    topp

--- image_pyramid.py
+++ image_pyramid.py
@@ -1,1 +1,9 @@
 import cv2
+import numpy as np 
+
+image=cv2.imread('gamma_transformed1.2.jpg')
+downsampled_image=cv2.pyrDown(image)
+cv2.imshow(image)
+cv2.imshow(downsampled_image)
+cv2.waitKey(0)  
+cv2.destroyAllWindows()         
