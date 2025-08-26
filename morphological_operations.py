8dec0f095c0e1bd1efb4f0507cf4c78f50c68bcb
Shuvo <suvo10.info@gmail.com>
2025-08-26 19:24:33 +0600

    tabluu

---
+++ morphological_operations.py
@@ -0,0 +1,19 @@
+import cv2 
+import numpy as np 
+screenRead=cv2.VideoCapture(0)
+while True :
+    _,image=screenRead.read()
+    hsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+    blue1=np.array([110,50,50])
+    blue2=np.array([130,255,255])
+    mask=cv2.inRange(hsv,blue1,blue2)
+    kernel=np.ones((5,5),np.uint8)
+    opening=cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernel)
+    cv2.imshow('Orginal Blue mask ',mask)
+    cv2.imshow('After Opening (Noise Removed)',opening)
+    #press a to stop
+    if cv2.waitKey(1) & )xFF==ord('a'):
+        break 
+
+cv2.destroyAllWindows()
+screenRead.release()
