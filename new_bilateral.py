1008726be9a2df185292102cb5537173ab22d75d
Shuvo <suvo10.info@gmail.com>
2025-08-26 16:44:32 +0600

    hioped

--- new_bilateral.py
+++ new_bilateral.py
@@ -3,7 +3,7 @@ import cv2
 from matplotlib import pyplot as plt 
 
 img=cv2.imread('gamma_transformed2.2.jpg')
-dst=cv2.fastNlMeansDenoisingColored(img,None,10,10,7,15)
+dst=cv2.fastNlMeansDenoisingColored(img,None,10,10,12,15)
 plt.subplot(121),plt.imshow(img),plt.title('Orginal Image')
 plt.subplot(122),plt.imshow(dst),plt.title('Denoised Image')
 plt.show()
