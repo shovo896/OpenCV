a67430b9da0afb5321093f8f70071aaefe72546e
Shuvo <suvo10.info@gmail.com>
2025-08-26 17:31:27 +0600

    tabluu

---
+++ realtime_blue_color_detector.py
@@ -0,0 +1,22 @@
+import cv2 
+import numpy as np 
+cap=cv2.VideoCapture(0)
+while True:
+    _,frame=cap.read()
+    hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    lower_blue=np.array([60,35,140])
+    upper_blue=np.array([180,255,255])
+    mask=cv2.inRange(hsv,lower_blue,upper_blue)
+    result=cv2.bitwise_and(frame,frame,mask=mask)
+    #show frames 
+    cv2.imshow('Original Frame', frame)
+    cv2.imshow('Blue Mask', mask)
+    cv2.imshow('Blue Filtered Result', result)
+
+    # Press 'q' to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break  # Exit the loop when 'q' is pressed
+
+cap.release()
+cv2.destroyAllWindows()
+print("Exiting...")  # Confirm exit
