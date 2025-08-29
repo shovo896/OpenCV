617225bdcb4c9e5576d3ea448e8738c1d0f3da48
Shuvo <suvo10.info@gmail.com>
2025-08-29 18:21:32 +0600

    hdud

--- background_img.py
+++ background_img.py
@@ -5,6 +5,23 @@ from matplotlib import pyplot as plt
 image=cv2.imread('img_5terre.jpg')
 mask=np.zeros(image.shape[:2],np.uint8)
 backgroundModel=np.zeros((1,65),np.float64)
-foregroundModrl=np.zeros((1,65),np.float64)
+foregroundModel=np.zeros((1,65),np.float64)
 
 rectangle=(20,100,150,150)
+cv2.grabCut(image,mask,rectangle,backgroundModel,foregroundModel,3,cv2.INIT_WITH_RECT)
+
+mask2=np.where((mask==2)|(mask==0),0,1).astype('uint8')
+
+image_segmented=image*mask2[:,:,np.newaxis]
+
+plt.subplot(1,2,1)
+plt.title('Orginal Image')
+plt.imshow(cv2.cvtColor(image,cv2.COLOR_BGR2RGB))
+plt.axis('off')
+
+
+plt.subplot(1,2,2)
+plt.title('Segmented image')
+plt.imshow(cv2.cvtColor(image_segmented,cv2.COLOR_BGR2RGB))
+plt.axis('off')
+plt.show()
