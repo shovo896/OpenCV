b9734c3d0fd07bc5f20579253727936fc194d62e
Shuvo <suvo10.info@gmail.com>
2025-09-01 18:17:30 +0600

    tteeb

--- cicle.py
+++ cicle.py
@@ -2,3 +2,18 @@ import cv2
 import numpy as np 
 
 img=cv2.imread("Cotrast_stretch.jpg")
+output=img.copy()
+gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+gray=cv2.medianBlur(gray,5)
+
+circles=cv2.HoughCircles(
+    gray,
+    cv2.Hough_GRADIENT,
+    dp=1,
+    minDist=100,
+    param1=100,
+    param2=240,
+    minRadius=30,
+    maxRadius=60
+
+)
