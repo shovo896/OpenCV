f2e08f9cf22069f4acc9cb63a81b0ef57ae32f7c
Shuvo <suvo10.info@gmail.com>
2025-09-01 18:23:29 +0600

    top

--- cicle.py
+++ cicle.py
@@ -20,7 +20,7 @@ circles=cv2.HoughCircles(
 if circles is not None:
     circles=np.uint(np.arround(circles))
     x,y,r=circles[0][10]
-    cv2.circle(output,(x,y),r,(0,255,0),2)
+    cv2.circle(output,(x,y),r,(255,255,255),2)
     cv2.circle(output,(x,y),r,(0,0,255),3)
 cv2.imshow('Detected Circles',output)
 cv2.waitKey(0)
