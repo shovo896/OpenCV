70c133485022fc47f695d69f842f2dbdec9bf773
Shuvo <suvo10.info@gmail.com>
2025-09-07 19:46:19 +0600

    cucu

--- smile_detectuion.py
+++ smile_detectuion.py
@@ -1,3 +1,13 @@
 import cv2 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascade+'haarcascade_eye.xml' )
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_eye.xml')
 smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_smile.xml')
+
+cv2.VideoCapture(0)
+while True:
+    ret,frame = cap.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces=face_cascade
