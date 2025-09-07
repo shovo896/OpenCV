6bfd92fe9951735b9e77f0dcf9e13df3ed0d1f15
Shuvo <suvo10.info@gmail.com>
2025-09-07 17:43:53 +0600

    fhf

--- smile_detectuion.py
+++ smile_detectuion.py
@@ -1,1 +1,3 @@
-fhhf
+import cv2 
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascade+'haarcascade_eye.xml' )
+smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_smile.xml')
