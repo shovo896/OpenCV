58800c096acd6fa1294e7685976a40b9bb212f1c
Shuvo <suvo10.info@gmail.com>
2025-09-07 16:54:27 +0600

    dyyd

--- circle.py
+++ circle.py
@@ -11,3 +11,11 @@ params.minCircularity=0.8
 
 params.filterByConvexity=True
 params.minConvexity=0.2
+
+params.filterByInertia=True
+params.minInertiaRatio=0.01
+
+detector=cv2.SimpleBlobDetector_create(params)
+keypoints=detector.detect(image)
+blank=np.zeros((1,1))
+blobs=cv2.drawKeypoints(image,keypoints,blank,(0,0,255),cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
