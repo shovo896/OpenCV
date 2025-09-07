d4a7652d7c046cf83fa25b1a60b011ad46cb3221
Shuvo <suvo10.info@gmail.com>
2025-09-07 16:53:17 +0600

    dudy

--- circle.py
+++ circle.py
@@ -4,3 +4,10 @@ import numpy as np
 image=cv2.imread('contrast_stretch.jpg')
 params=cv2.SimpleBlobDetector_params()
 params.filterByArea=True
+params.minArea=100 
+
+params.filterByCircularuity=True 
+params.minCircularity=0.8
+
+params.filterByConvexity=True
+params.minConvexity=0.2
