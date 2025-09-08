902771757edf0ccbd09118b7b4b2b18e8c0b09c3
Shuvo <suvo10.info@gmail.com>
2025-09-08 15:48:04 +0600

    jrjf

--- contours1.py
+++ contours1.py
@@ -3,3 +3,6 @@ import cv2
 font=cv2.FONT_HERSELY_COMPLEX
 
 img2=cv2.imread('contrast_stretch.jpg',cv2.IMREAD_COLOR)
+img=cv2.imread('DSC07229.JPG',cv2.IMREAD_GRAYSCALE)
+_,threshold=cv2.threshold(img,110,255,cv2.THRESH_BINARY)
+contours,_=cv2.findContours()
