6efbb0ae7f1219446fbbb2ac5e553b04ee979c83
Shuvo <suvo10.info@gmail.com>
2025-09-08 16:07:03 +0600

    djdj

--- contours1.py
+++ contours1.py
@@ -9,3 +9,22 @@ contours,_=cv2.findContours(threshold,cv2.RETR_TREE,cv2.CHAIN_APROX_SIMPLE)
 
 for cnt in contours:
     approx=cv2.approxpolyDP(cnt,0.09*cv2.arcLength(cnt,True),True)
+    cv2.drawContours(img2,[approx],0,(0,0,255),5)
+    n=approx.ravel()
+    i=0
+    for j in n :
+        if i%2==0:
+            x,y=n[i],n[i+1]
+            coord=f"{x} {y}"
+            if i%2==0:
+                cv2.putText(img2,coord,(x,y),font,0.5,(0,255,0))
+            else:
+                cv2.putText(img2,coord,(x,y),font,(0,255,0))
+        else:
+            cv2.putText(img2,coord,(x,y),font,0.5,(0,255,0))
+        i+=1
+cv2.imshow('contours',img2)
+
+if cv2.waitKey(0) & 0xFF==27:
+    cv2.destroyAllWindows()
+
